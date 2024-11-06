e5bb42fe682e09f5db476ce9896036f20ecd026a
rizkyridwan-id <rizkyridwan.id@gmail.com>
2024-11-06 14:54:33 +0700

    change port to 5000

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const ClientError = require("./exceptions/ClientError");
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 3000,
+    port: 5000,
     host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
